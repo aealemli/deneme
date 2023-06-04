0286a38c54782e7d8dc78ea087e3214b2fc35912
Abdullah Ege Alemli <aealemli@gmail.com>
2023-06-04 04:42:38 +0300

    deneme

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:chatapp_firebase/pages/home_page.dart';
+import 'package:chatapp_firebase/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: Constants.apiKey,
+        appId: Constants.appId,
+        messagingSenderId: Constants.messagingSenderId,
+        projectId: Constants.projectId,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Constants().primaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:700532709077:android:83f92eb01d53b59f6af45c";
+  static String apiKey = "AIzaSyB08D3wT5Vm-CLETAj9JWzRSaxukUxfaVc";
+  static String messagingSenderId = "700532709077";
+  static String projectId = "halichat";
+  final primaryColor = const Color(0xFFee7b64);
+}
